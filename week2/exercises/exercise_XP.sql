ae88a0a0bcd624d714ade81b075f26709160f918
Nossayba-dev <noussayba.abbara99@gmail.com>
2025-05-12 15:25:11 +0100

    DAY1

--- week2/exercises/exercise_XP.sql
+++ week2/exercises/exercise_XP.sql
@@ -11,13 +11,13 @@ CREATE TABLE customers (
 	first_name varchar(50),
 	last_name varchar(50)
 );
-
+-- 1
 INSERT INTO items (items,price)
 VALUES 
 ('Small Desk' ,100 ),
 ('Large desk' ,300),
 ('Fan' ,80);
-
+-- 2
 INSERT INTO customers(first_name , last_name)
 VALUES 
 ( 'Greg' , 'Jones'),
@@ -25,15 +25,21 @@ VALUES
 ('Scott' , 'Scott'),
 ('Trevor' , 'Green'),
 ('Melanie' , 'Johnson');
+-- 3.1
 SELECT * FROM items;
+-- 3.2
 SELECT * FROM items
--- WHERE price>80;
+WHERE price>80;
+-- 3.3
 SELECT * FROM items
 WHERE price<=300;
+-- 3.4
 SELECT * FROM customers
 WHERE last_name = 'Smith'; --we well have nothing 
+-- 3.5
 SELECT * FROM customers
 WHERE last_name = 'Jones';
+-- 3.6
 SELECT * FROM customers
 WHERE last_name != 'Scott';
 
