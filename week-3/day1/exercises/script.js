27d7132b8a81d9f100f88f69927324c86bc52b00
Nossayba-dev <noussayba.abbara99@gmail.com>
2025-05-20 17:53:26 +0100

    geeks.geeks

--- week-3/day1/exercises/script.js
+++ week-3/day1/exercises/script.js
@@ -51,15 +51,68 @@ for( let i in colors){
 
 
 // Exercise 3 : Repeat the question
-let number=parseInt(prompt("enter a number"))
-
-// Exercise 4 : Building Management
 let number
 do{
      number = parseInt(prompt("enter a number"));
 }
 while(number < 10);
-// Exercise 5 : Family
 
+// Exercise 4 : Building Management
+const building = {
+    numberOfFloors: 4,
+    numberOfAptByFloor: {
+        firstFloor: 3,
+        secondFloor: 4,
+        thirdFloor: 9,
+        fourthFloor: 2,
+    },
+    nameOfTenants: ["Sarah", "Dan", "David"],
+    numberOfRoomsAndRent:  {
+        sarah: [3, 990],
+        dan:  [4, 1000],
+        david: [1, 500],
+    },
+}
+
+console.log(building.numberOfFloors);
+console.log(`${building.numberOfAptByFloor.firstFloor} and ${building.numberOfAptByFloor.secondFloor}`);
+console.log(`${building.nameOfTenants.slice(1,2)} : ${building.numberOfRoomsAndRent.dan}`);
+sum = building.numberOfRoomsAndRent.sarah[1] + building.numberOfRoomsAndRent.david[1];
+if(sum > building.numberOfRoomsAndRent.dan[1]){
+    building.numberOfRoomsAndRent.dan[1] = 1200
+};
+console.log(building.numberOfRoomsAndRent.dan[1]);
+// Exercise 5 : Family
+const Family={
+    father :"Youssef",
+    mather :"meriem",
+    bigsis : "Noussayba",
+    bigbro :"Oubay",
+    letterbro : "Abdoelrrahman",
+    lettersis : "Razan"
+}
+for (let key in Family) {
+  console.log(key);
+}
+
+for(let key in Family){
+    console.log(Family[key]);
+}
 // Exercise 6 : Rudolf
+const details = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'reindeer'
+}
+let str=""
+for(let i in details ){
+    str += `${i} ${details[i]} `;
+}
+console.log(str)
+
 // Exercise 7 : Secret Group
+const names = ["Jack", "Philip", "Sarah", "Amanda", "Bernard", "Kyle"];
+const initials = names.map(name => name[0]);
+const sortedInitials = initials.sort();
+const secretSocietyName = sortedInitials.join("");
+console.log(secretSocietyName); 
