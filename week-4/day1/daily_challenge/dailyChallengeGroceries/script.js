955e53baf14b65bfa2796046f5f5b79387abe1e4
Nossayba-dev <noussayba.abbara99@gmail.com>
2025-06-08 18:16:13 +0100

    geeks.geeks

--- week-4/day1/daily_challenge/dailyChallengeGroceries/script.js
+++ week-4/day1/daily_challenge/dailyChallengeGroceries/script.js
@@ -8,27 +8,23 @@ const groceries = {
         paid : true,
         meansOfPayment : ["cash", "creditCard"]
     }
+}
+ const displayGroceries=()=>{
+    groceries.fruits.forEach(element => console.log(element))
 };
+ displayGroceries();
+ const cloneGroceries=()=>{
+    let user = client; 
+     client = 'Betty';
+    console.log(user); 
+    console.log(client); 
+    // non car on a juste copie la valeur de client dans user 
 
-const displayGroceries = () => {
-    groceries.fruits.forEach(fruit => console.log(fruit));
-};
-
-const cloneGroceries = () => {
-    let user = client;
-    client = "Betty";
-    console.log("Client:", client); 
-    console.log("User:", user);    
-
-    let shopping = groceries;
-
-    shopping.totalPrice = "35$";
-    
-    shopping.other.paid = false;
-
-    console.log("groceries.totalPrice:", groceries.totalPrice); 
-    console.log("groceries.other.paid:", groceries.other.paid); 
-};
-
-displayGroceries();   
-cloneGroceries();     
+    let shopping =groceries;
+    groceries.totalPrice='35$';
+    console.log(shopping.totalPrice)// the same reson
+    groceries.other.paid=false;
+    console.log(shopping.other.paid)
+    // oui car shopping = groceries signifie que shopping pointe vers le même objet.
+     }
+ cloneGroceries()
